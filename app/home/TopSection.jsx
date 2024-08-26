83d8f171a50b1fcbd91dbc9c996550ca254cf3ad
amitgiti137 <amit1317jmp@gmail.com>
2024-08-26 19:16:44 +0530

    new changes

--- app/home/TopSection.jsx
+++ app/home/TopSection.jsx
@@ -22,7 +22,7 @@ function TopSection() {
                         </p>
                     </div>
                     <div>
-                        <Button className='rounded-full bg-[#11009E] px-10 py-2 text-white'>Get Free Consultation!</Button>
+                        <Button className='rounded-full bg-[#11009E] px-10 py-2 text-white hover:bg-[#513eff]'>Get Free Consultation!</Button>
                     </div>
                 </div>
                 <div className='w-[100%] lg:w-[50%]'>
