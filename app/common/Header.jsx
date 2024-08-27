8b4e943cdfc736feaaf5586d2aa66bb34043b163
shahrukhdigitalwolf <mohammadshahrukh.digitalwolf@gmail.com>
2024-08-27 16:56:30 +0530

    navbar space

--- app/common/Header.jsx
+++ app/common/Header.jsx
@@ -102,13 +102,13 @@ export default function Header() {
             </PopoverButton>
             <PopoverPanel
               static
-              className={`absolute -left-72 top-full z-[99] w-[50rem] max-w-[50rem] overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition-opacity duration-300 ${popoverOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}
+              className={`absolute -left-80 top-full z-[99] w-[50rem] max-w-[50rem] overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition-opacity duration-300 ${popoverOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}
             >
               <div className="p-4 grid grid-cols-2">
                 {products.map((item) => (
                   <div
                     key={item.name}
-                    className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                    className="group relative flex items-center gap-x-6 rounded-lg px-4 py-3 text-sm leading-6 hover:bg-gray-50"
                   >
                     <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
                       <item.icon aria-hidden="true" className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" />
